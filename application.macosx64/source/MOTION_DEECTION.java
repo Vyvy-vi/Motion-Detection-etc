366a87a31121569c0016764f0f7c0d2c9945dd6a
Vyom Jain <vyom.j@protonmail.com>
2020-06-08 08:00:13 +0530

    macosapp(motion_track)

---
+++ application.macosx64/source/MOTION_DEECTION.java
@@ -0,0 +1,134 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import processing.video.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class MOTION_DEECTION extends PApplet {
+
+
+//motion tracking
+Capture video;
+PImage prev;
+
+float threshold = 25;
+
+float motionX = 0;
+float motionY = 0;
+
+float lerpX = 0;
+float lerpY = 0;
+
+
+public void setup() {
+  
+  String[] cameras = Capture.list();
+  printArray(cameras);
+  video = new Capture(this, cameras[3]);
+  video.start();
+  prev = createImage(640, 360, RGB);
+  // Start off tracking for red
+}
+
+
+public void mousePressed() {
+}
+
+public void captureEvent(Capture video) {
+  prev.copy(video, 0, 0, video.width, video.height, 0, 0, prev.width, prev.height);
+  prev.updatePixels();
+  video.read();
+}
+
+public void draw() {
+  video.loadPixels();
+  prev.loadPixels();
+  image(video, 0, 0);
+
+  //threshold = map(mouseX, 0, width, 0, 100);
+  threshold = 50;
+
+
+  int count = 0;
+  
+  float avgX = 0;
+  float avgY = 0;
+
+  loadPixels();
+  // Begin loop to walk through every pixel
+  for (int x = 0; x < video.width; x++ ) {
+    for (int y = 0; y < video.height; y++ ) {
+      int loc = x + y * video.width;
+      // What is current color
+      int currentColor = video.pixels[loc];
+      float r1 = red(currentColor);
+      float g1 = green(currentColor);
+      float b1 = blue(currentColor);
+      int prevColor = prev.pixels[loc];
+      float r2 = red(prevColor);
+      float g2 = green(prevColor);
+      float b2 = blue(prevColor);
+
+      float d = distSq(r1, g1, b1, r2, g2, b2); 
+
+      if (d > threshold*threshold) {
+        //stroke(255);
+        //strokeWeight(1);
+        //point(x, y);
+        avgX += x;
+        avgY += y;
+        count++;
+        pixels[loc] = color(255);
+      } else {
+        pixels[loc] = color(0);
+      }
+    }
+  }
+  updatePixels();
+
+  // We only consider the color found if its color distance is less than 10. 
+  // This threshold of 10 is arbitrary and you can adjust this number depending on how accurate you require the tracking to be.
+  if (count > 200) { 
+    motionX = avgX / count;
+    motionY = avgY / count;
+    // Draw a circle at the tracked pixel
+  }
+  
+  lerpX = lerp(lerpX, motionX, 0.1f); 
+  lerpY = lerp(lerpY, motionY, 0.1f); 
+  
+  fill(0, 0, 255);
+  strokeWeight(2.0f);
+  stroke(0);
+  ellipse(lerpX, lerpY, 36, 36);
+
+  //image(video, 0, 0, 100, 100);
+  //image(prev, 100, 0, 100, 100);
+
+  //println(mouseX, threshold);
+}
+
+public float distSq(float x1, float y1, float z1, float x2, float y2, float z2) {
+  float d = (x2-x1)*(x2-x1) + (y2-y1)*(y2-y1) +(z2-z1)*(z2-z1);
+  return d;
+}
+  public void settings() {  size(640, 360); }
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "--present", "--window-color=#666666", "--stop-color=#cccccc", "MOTION_DEECTION" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
